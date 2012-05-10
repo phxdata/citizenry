9b5df67ea3172718f637089fa7ff2ceeb18a6dad
Marc Chung <mchung@gmail.com>
2012-05-10 16:08:57 -0700

    Remove hoptoad

--- config/initializers/hoptoad.rb
+++ config/initializers/hoptoad.rb
@@ -1,5 +1,5 @@
-unless SETTINGS['hoptoad'] == 'seekrit!'
-  HoptoadNotifier.configure do |config|
-    config.api_key = SETTINGS['hoptoad']
-  end
-end
+# unless SETTINGS['hoptoad'] == 'seekrit!'
+#   HoptoadNotifier.configure do |config|
+#     config.api_key = SETTINGS['hoptoad']
+#   end
+# end
